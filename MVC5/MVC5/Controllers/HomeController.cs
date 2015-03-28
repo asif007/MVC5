afb3274d23985465c3189266c896a72951c186fc
asif007 <ibnaasif@gmail.com>
2015-03-28 01:33:05 +0000

    kendo

--- MVC5/MVC5/Controllers/HomeController.cs
+++ MVC5/MVC5/Controllers/HomeController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MVC5.Models;
 namespace MVC5.Controllers
 {
     public class HomeController : Controller
     {
+        TSQL2012Entities db = new TSQL2012Entities();
         public ActionResult Index()
         {
             return View();
@@ -31,6 +32,23 @@ namespace MVC5.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public PartialViewResult All()
+        {
+            List<Employee> emp = db.Employees.ToList();
+            return PartialView("_AjaxPartialView", emp);
+        }
+        public PartialViewResult Top3()
+        {
+            List<Employee> emp = new List<Employee>();
+            emp = db.Employees.OrderBy(r => r.empid).Take(3).ToList();
+            return PartialView("_AjaxPartialView", emp);
+        }
+        public PartialViewResult Bottom3()
+        {
+            List<Employee> emp = new List<Employee>();
+            emp = db.Employees.OrderByDescending(r => r.empid).Take(3).ToList();
+            return PartialView("_AjaxPartialView", emp);
         }
     }
 }
